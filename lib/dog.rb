a063021b3966d100001e91b2ee1e9bac5fff0fe9
Miryam Rabinowitz <miryamrabinowitz@gmail.com>
2020-04-12 01:51:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,5 @@
 class Dog
+
   def name=(dogs_name)
     @this_dogs_name = dogs_name
   end
@@ -6,8 +7,7 @@ class Dog
   def name
     @this_dogs_name
   end
+end 
 
 fido = Dog.new
 fido.name = "fido"
-
-end
